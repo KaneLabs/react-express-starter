4608ec4081438db90e8e1626b7617e9e27224591
dwleonhardt <dwleonhardt@gmail.com>
2017-07-12 12:17:53 -0600

    added splash component

--- App/index.jsx
+++ App/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // attaches react app to html5
 import './index.css'; // styling for body
+import Splash from './splash'
 
 function App () {
-  return <div>React Express Lite</div>
-}
+  return (
+    <splash />
+  )
 
-ReactDOM.render(<App />, document.querySelector('.app'));
+}
---
+++ App/splash.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Splash () => {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Splash;
